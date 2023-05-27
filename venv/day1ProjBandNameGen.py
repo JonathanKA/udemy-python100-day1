2fcb8b09a0423c7b6763364bbfca2e860ff53bf4
Jonathan <8617943+JonathanKA@users.noreply.github.com>
2023-05-26 21:08:45 -0400

    Added input to new line functionality

--- venv/day1ProjBandNameGen.py
+++ venv/day1ProjBandNameGen.py
@@ -1,5 +1,5 @@
 #day1-100days of code udemy Dr.Angela
-name = input("What's the name of the city you grew up in? ")
-animal = input("What's your pet's name? ")
+name = input("What's the name of the city you grew up in?\n")
+animal = input("What's your pet's name?\n")
 
 print('Your band name is ' + name + ' ' + animal + '!')
